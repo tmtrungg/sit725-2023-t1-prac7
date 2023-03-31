77af96a199290c0e8a31362ab1be4e3e2967c7fd
Trung M. Tran <trantrung951@gmail.com>
2023-03-31 14:09:50 +1100

    Final version
    The final version of demo

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require("express")
-var app = express()
+var express = require('express');
+var app = express();
+var port = process.env.port || 3000;
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-var port = process.env.port || 3000;
 
 app.listen(port,()=>{
-    console.log("App listening to: "+ port)
-})
+    console.log('App listening to: ' + port);
+});
